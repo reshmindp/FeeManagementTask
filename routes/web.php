3bc33b43ec6344c3e8169e00749bb2165fc830a0
Reshmin DP <reshminsouparnam@gmail.com>
2025-02-12 13:18:40 +0530

    auth and assets

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::post('/login', [AuthController::class, 'login'])->name('loginSubmit');
+
+Route::middleware(['guest'])->group(function () {
+    Route::get('/', [AuthController::class, 'adminLogin'])->name('login');
+    
+    Route::post('/login', [AuthController::class, 'login'])->name('loginSubmit');
 });
